10bb60239ca2101720477999182e1644c40bd095
Sören Gutzeit <soeren.gutzeit@gmail.com>
2017-08-01 22:21:02 +0200

    created twitterListender as module. added twitterlistener to jammeshead

--- jammes-head/start.py
+++ jammes-head/start.py
@@ -4,6 +4,8 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 from libs import TwitterHelper as th
+from time import sleep
+from libs import TwitterListener as tl
 
 DEBUG = False
 
@@ -27,24 +29,54 @@ twitter = th.TwitterHelper(config.get('Twitter', 'consumer_key'),
                            config.get('Twitter', 'access_token_secret'),
                            debug=DEBUG)
 
-# Logger
+# read and create logger configs
 app_name = config.get('Jammes-Head', 'app-name', fallback='Jammes-Head')
 log_level = config.get('Log', 'level', fallback='INFO')
 log_file = config.get('Log', 'file', fallback='/tmp/jammes-head.log')
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+# create logger
 logging.basicConfig(filename=log_file, level=logging.getLevelName(log_level))
 logger = logging.getLogger(app_name)
+
+# create file handler
 fh = RotatingFileHandler(log_file, maxBytes=10000000, backupCount=5)
+fh.setLevel(logging.getLevelName(log_level))
+
+# create console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter for both handlers
 formatter = logging.Formatter(log_format)
 fh.setFormatter(formatter)
-fh.setLevel(logging.getLevelName(log_level))
+ch.setFormatter(formatter)
+
+# adding handler
 logger.addHandler(fh)
+logger.addHandler(ch)
 
 if config.getboolean('Twitter', 'log_via_twitter', fallback=False):
     twitter.active_twitter_logger_for(app_name, config.get('Twitter', 'log_target_user', fallback=''))
 
 
 def main():
+
+    # init twitter listener
+    twitter_listener = tl.TwitterListener(config.get('Twitter', 'consumer_key'),
+                                          config.get('Twitter', 'consumer_secret'),
+                                          config.get('Twitter', 'access_token'),
+                                          config.get('Twitter', 'access_token_secret'),
+                                          logger)
+    twitter_listener.setName("TwitterListener")
+    twitter_listener.start()
+
+    logger.info("TwitterListener initialized.")
+
+    # TODO ?
+
+    while True:
+        sleep(1)
     pass
 
 if __name__ == "__main__":
---
+++ libs/TwitterListener.py
@@ -0,0 +1,80 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy import API
+
+from tweepy.streaming import StreamListener
+from threading import Thread
+import json
+import logging
+
+
+# These values are appropriately filled in the code
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+class StdOutListener( StreamListener ):
+
+    def __init__( self, logger ):
+        self.tweetCount = 0
+        self.logger = logger
+
+    def on_connect( self ):
+        self.logger.info("Connection established!!")
+
+    def on_disconnect( self, notice ):
+        self.logger.error("Connection lost!! : {}".format(notice))
+
+    def on_data( self, status ):
+        self.logger.info("Entered on_data()")
+        obj= json.loads(status)
+
+        if 'direct_message' in obj:
+            dm = obj['direct_message']
+            sender = dm['sender']
+            self.logger.info("<{}> {}: {}".format(dm['created_at'], sender['screen_name'], dm['text']))
+
+        return True
+
+    def on_direct_message( self, status ):
+        print("Entered on_direct_message()")
+        try:
+            print(status, flush = True)
+            return True
+        except BaseException as e:
+            self.logger.error("Failed on_direct_message(): {}".format(str(e)))
+
+    def on_error( self, status ):
+        print(status)
+
+
+class TwitterListener (Thread):
+
+    def __init__(self, consumer_key, consumer_secret, access_key, access_secret, logger):
+        super(TwitterListener, self).__init__()
+        self.logger = logger
+
+        try:
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth.secure = True
+            self.auth.set_access_token(access_key, access_secret)
+
+            api = API(self.auth)
+
+            # If the authentication was successful, you should
+            # see the name of the account print out
+            self.logger.info("Successfully authenticate with twitter account '{}'.".format(api.me().name))
+
+        except BaseException as e:
+            self.logger.error("Error in main()", e)
+
+    def run(self):
+        try:
+            stream = Stream(self.auth, StdOutListener(self.logger) )
+
+            stream.userstream()
+
+        except BaseException as e:
+            self.logger.error("Error in main()", e)
